89cc0f940e82874f9fe2b6f9a066e72eb7aa69c3
taninon <tanimichi@morality-line.com>
2024-01-28 02:03:02 +0900

    Acd

---
+++ Assets/Script/Player_test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Player_test : MonoBehaviour
+{
+	[SerializeField] private float _speed;
+
+	private void Update()
+	{
+		if (CommandManager.Instance.Locked) {
+			return;
+		}
+
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		var move = new MoveCommand(this.transform,h,v,_speed);
+		move.Execute();
+
+		CommandManager.Instance.AddCommand(move);
+	}
+
+}
